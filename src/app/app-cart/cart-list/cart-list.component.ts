4d3c75c2ec0ecf7a21033f8b6f7129457437ee5d
ambikesh1982 <ambikesh1982@gmail.com>
2018-06-20 10:56:15 +0800

    app-cart works now

--- src/app/app-cart/cart-list/cart-list.component.ts
+++ src/app/app-cart/cart-list/cart-list.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit, Input, EventEmitter, Output } from '@angular/core';
 import { Observable } from 'rxjs';
 import { AppCartService } from '../app-cart.service';
+import { tap } from 'rxjs/operators';
 
 @Component({
   selector: 'app-cart-list',
@@ -13,31 +14,37 @@ export class CartListComponent implements OnInit {
   @Input() cartID: string;
   @Output() qtyChange = new EventEmitter();
   @Output() deleteItem = new EventEmitter();
+  @Output() removeSeller = new EventEmitter();
 
   cartItemsBySellerID$: Observable<ICartItem[]>;
 
 
-  constructor( private cartService: AppCartService) {
-   }
+  constructor(private cartService: AppCartService) { }
 
   ngOnInit() {
-    // this.cartItemsBySellerID = this.cartItems.filter(item => item.seller.id === this.sellerID);
-    this.cartItemsBySellerID$ = this.cartService.getCartItemsBySeller$(this.cartID, this.sellerID);
+    this.cartItemsBySellerID$ = this.cartService.getCartItemsBySeller$(this.cartID, this.sellerID)
+      .pipe(
+        tap(items => {
+          if (items.length === 0) {
+            this.removeSeller.emit(this.sellerID);
+          }
+        })
+      );
   }
 
   icrementItemCount(item: ICartItem) {
-    console.log('TODO: updateItemQuantity(path: string, count: number): ', item);
-    this.qtyChange.emit({count: 1, item: item});
+    console.log('updateItemQuantity(path: string, count: number): ', item);
+    this.qtyChange.emit({ count: 1, item: item });
   }
 
   decrementItemCount(item: ICartItem) {
     this.qtyChange.emit({ count: -1, item: item });
-    console.log('TODO: updateItemQuantity(path: string, count: number): ', item);
+    console.log('updateItemQuantity(path: string, count: number): ', item);
   }
 
   removeItem(id: string) {
     this.deleteItem.emit(id);
-    console.log('TODO: removeItemFromCart(id: string): ', id);
+    console.log('removeItemFromCart(id: string): ', id);
   }
 
 }
